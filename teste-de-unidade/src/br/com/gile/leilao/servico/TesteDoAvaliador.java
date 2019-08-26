8cda07d7b23e1b2384947ba4c4b72728e86783fa
Gileade Trindade Silva <gileade.trindade@gmail.com>
2019-08-26 18:21:12 -0300

    Adicionando método de Média e Testes

--- teste-de-unidade/src/br/com/gile/leilao/servico/TesteDoAvaliador.java
+++ teste-de-unidade/src/br/com/gile/leilao/servico/TesteDoAvaliador.java
@@ -11,27 +11,64 @@ public class TesteDoAvaliador {
 
 	@Test
 	public void deveEntenderLancesEmOrdemCrescente() {
-		//Cenário: 3 lances em ordem crescente
+		// Cenário: 3 lances em ordem crescente
 		Usuario joao = new Usuario("João");
 		Usuario jose = new Usuario("José");
 		Usuario maria = new Usuario("Maria");
-		
+
 		Leilao leilao = new Leilao("Playstation 3 novo");
-		
-		leilao.propoe(new Lance(joao,250.0));
-		leilao.propoe(new Lance(jose,300.0));
-		leilao.propoe(new Lance(maria,400.0));
-		
-		//executando a ação
+
+		leilao.propoe(new Lance(joao, 250.0));
+		leilao.propoe(new Lance(jose, 300.0));
+		leilao.propoe(new Lance(maria, 400.0));
+
+		// executando a ação
 		Avaliador leiloeiro = new Avaliador();
 		leiloeiro.avalia(leilao);
-		
-		//comparando a saída com o esperado
+
+		// comparando a saída com o esperado
 		double maiorEsperado = 400;
 		double menorEsperado = 250;
+
+		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);// Deve imprimir 400
+		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);// Deve imprimir 250
+	}
+
+	@Test
+	public void deveCalcularAMedia() {
+		// Cenário: 3 lances em ordem crescente
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("José");
+		Usuario maria = new Usuario("Maria");
+
+		Leilao leilao = new Leilao("Playstation 3 novo");
+
+		leilao.propoe(new Lance(joao, 300));
+		leilao.propoe(new Lance(jose, 400.0));
+		leilao.propoe(new Lance(maria, 500.0));
+
+		// executando a ação
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+
+		Assert.assertEquals(400, leiloeiro.getMedia(), 0.00001);// Deve imprimir 250
+	}
+
+	@Test
+	public void testaZeroDeMedia() {
+		// Cenário: 3 lances em ordem crescente
+		Usuario joao = new Usuario("João");
+
+		Leilao leilao = new Leilao("Playstation 3 novo");
+
+		leilao.propoe(new Lance(joao, 0));
 		
-		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(),0.00001);//Deve imprimir 400
-		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(),0.00001);//Deve imprimir 250
+		// executando a ação
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+
+		Assert.assertEquals(0, leiloeiro.getMedia(), 0.00001);// Deve imprimir 250
+
 	}
 
 }
