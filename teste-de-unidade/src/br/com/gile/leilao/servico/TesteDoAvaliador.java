542d7aa83699aa9ad795468365c9d807fc58720a
Gileade Trindade Silva <gileade.trindade@gmail.com>
2019-08-26 13:25:31 -0300

    3 passos para testar

--- teste-de-unidade/src/br/com/gile/leilao/servico/TesteDoAvaliador.java
+++ teste-de-unidade/src/br/com/gile/leilao/servico/TesteDoAvaliador.java
@@ -7,6 +7,7 @@ import br.com.gile.leilao.dominio.Usuario;
 public class TesteDoAvaliador {
 
 	public static void main(String[] args) {
+		//Cenário: 3 lances em ordem crescente
 		Usuario joao = new Usuario("João");
 		Usuario jose = new Usuario("José");
 		Usuario maria = new Usuario("Maria");
@@ -17,9 +18,11 @@ public class TesteDoAvaliador {
 		leilao.propoe(new Lance(jose,300.0));
 		leilao.propoe(new Lance(maria,400.0));
 		
+		//executando a ação
 		Avaliador leiloeiro = new Avaliador();
 		leiloeiro.avalia(leilao);
 		
+		//exibindo a saída
 		System.out.println(leiloeiro.getMaiorLance());//Deve imprimir 400
 		System.out.println(leiloeiro.getMenorLance());//Deve imprimir 250
 	}
