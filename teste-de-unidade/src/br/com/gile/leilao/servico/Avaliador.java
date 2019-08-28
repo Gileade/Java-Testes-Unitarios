00c3d58cf5af9fcae232f64ffd6f96ee135cdb05
Gileade Trindade Silva <gileade.trindade@gmail.com>
2019-08-28 12:54:37 -0300

    Inserindo metodo de pegar 3 maiores lances

--- teste-de-unidade/src/br/com/gile/leilao/servico/Avaliador.java
+++ teste-de-unidade/src/br/com/gile/leilao/servico/Avaliador.java
@@ -1,5 +1,10 @@
 package br.com.gile.leilao.servico;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import br.com.gile.leilao.dominio.Lance;
 import br.com.gile.leilao.dominio.Leilao;
 
@@ -8,7 +13,7 @@ public class Avaliador {
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
 	private double menorDeTodos = Double.POSITIVE_INFINITY;
 	private double media = 0;
-	
+	private List<Lance> maiores;
 	
 	public void avalia(Leilao leilao) {
 		double total = 0;
@@ -17,12 +22,31 @@ public class Avaliador {
 			if (lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
 			total+=lance.getValor();
 		}
+		
 		if(total == 0) {
 			media = 0;
 			return;
 		}
 		
 		media = total /leilao.getLances().size();
+		
+		pegaOsMaioresNo(leilao);
+	}
+	
+	private void pegaOsMaioresNo(Leilao leilao) {
+        maiores = new ArrayList<Lance>(leilao.getLances());
+        Collections.sort(maiores, new Comparator<Lance>() {
+            public int compare(Lance o1, Lance o2) {
+                if(o1.getValor() < o2.getValor()) return 1;
+                if(o1.getValor() > o2.getValor()) return -1;
+                return 0;
+            }
+        });
+        maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
+    }
+	
+	public List<Lance> getTresMaiores(){
+		return this.maiores;
 	}
 	
 	public double getMaiorLance(){
