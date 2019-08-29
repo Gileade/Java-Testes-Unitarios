e7bd4be1e26186fc46736bd974f5822499b2efe0
Gileade Trindade Silva <gileade.trindade@gmail.com>
2019-08-29 13:13:17 -0300

    Inserindo novos testes no FiltroTest

--- teste-de-unidade/src/br/com/gile/leilao/testes/TesteFiltroDeLance.java
+++ teste-de-unidade/src/br/com/gile/leilao/testes/TesteFiltroDeLance.java
@@ -42,4 +42,59 @@ public class TesteFiltroDeLance {
         assertEquals(1, resultado.size());
         assertEquals(600, resultado.get(0).getValor(), 0.00001);
     }
+    
+    @Test
+    public void deveSelecionarLancesMaioresQue5000() {
+        Usuario joao = new Usuario("Joao");
+
+        FiltroDeLances filtro = new FiltroDeLances();
+        List<Lance> resultado = filtro.filtra(Arrays.asList(
+                new Lance(joao,6000), 
+                new Lance(joao,5000), 
+                new Lance(joao,700), 
+                new Lance(joao, 8000)));
+
+        assertEquals(2, resultado.size());
+        assertEquals(6000, resultado.get(0).getValor(), 0.00001);
+        assertEquals(8000, resultado.get(1).getValor(), 0.00001);
+    }
+    
+    @Test
+    public void deveEliminarLancesMenoresQue500() {
+        Usuario joao = new Usuario("Joao");
+
+        FiltroDeLances filtro = new FiltroDeLances();
+        List<Lance> resultado = filtro.filtra(Arrays.asList(
+                new Lance(joao,500), 
+                new Lance(joao,400), 
+                new Lance(joao,300)));
+
+        assertEquals(0, resultado.size());
+    }
+    
+    @Test
+    public void deveEliminarEntre700E1000() {
+        Usuario joao = new Usuario("Joao");
+
+        FiltroDeLances filtro = new FiltroDeLances();
+        List<Lance> resultado = filtro.filtra(Arrays.asList(
+                new Lance(joao, 800),
+                new Lance(joao, 1000),
+                new Lance(joao, 700),
+                new Lance(joao, 900)));
+
+        assertEquals(0, resultado.size());
+    }
+
+    @Test
+    public void deveEliminarEntre3000E5000() {
+        Usuario joao = new Usuario("Joao");
+
+        FiltroDeLances filtro = new FiltroDeLances();
+        List<Lance> resultado = filtro.filtra(Arrays.asList(
+                new Lance(joao,4000), 
+                new Lance(joao, 3500)));
+
+        assertEquals(0, resultado.size());
+    }
 }
