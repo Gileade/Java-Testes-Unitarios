4e835781d941a8866bd28d1dc831d925b9626bc8
Gileade Trindade Silva <gileade.trindade@gmail.com>
2019-09-16 21:30:31 -0300

    Add Builder para os testes

---
+++ teste-de-unidade/src/br/com/gile/leilao/builder/CriadorDeLeilao.java
@@ -0,0 +1,25 @@
+package br.com.gile.leilao.builder;
+
+import br.com.gile.leilao.dominio.Lance;
+import br.com.gile.leilao.dominio.Leilao;
+import br.com.gile.leilao.dominio.Usuario;
+
+public class CriadorDeLeilao {
+
+	private Leilao leilao;
+
+	public CriadorDeLeilao para(String descricao) {
+		this.leilao = new Leilao(descricao);
+		return this;
+	}
+
+	public CriadorDeLeilao lance(Usuario usuario, double valor) {
+		leilao.propoe(new Lance(usuario,valor));
+		return this;
+	}
+
+	public Leilao constroi() {
+		return leilao;
+	}
+
+}
